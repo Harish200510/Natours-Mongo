6cc1d4754a4197065863dd06fc045f5719fe78b8
Harish200510 <harish.venkatesan2005@gmail.com>
2025-07-03 22:14:46 +0530

    init

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const dotenv=require('dotenv')
+const mongoose=require('mongoose')
 dotenv.config({path:'./config.env'})
 const app=require('./app')
 
+const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
 
-console.log(process.env.NODE_ENV)
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false 
+}).then(con=>{
+    console.log("Mongodb connected")
+})
+
 
 const port=process.env.PORT || 8000;
 app.listen(port,()=>{
