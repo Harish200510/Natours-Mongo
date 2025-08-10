58a65e905b854da1686406972ca93fab29d38fba
Harish200510 <harish.venkatesan2005@gmail.com>
2025-08-10 11:16:29 +0500

    finished Authentication

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 const express=require('express')
 const morgan=require('morgan');
 const rateLimit=require('express-rate-limit')
+const qs=require('qs')
+const helmet=require('helmet')
+const mongoSanitize = require('express-mongo-sanitize');
+const xss=require('xss-clean')
+const hpp=require('hpp')
+
+
 const AppError=require('./utils/appError')
 const globalErrorHandler=require('./controllers/errorController')
 const tourRouter=require('./Routes/tourRoutes')
-const userRouter=require('./Routes/userRoutes')
-const qs=require('qs')
+const userRouter=require('./Routes/userRoutes');
+const ExpressMongoSanitize = require('express-mongo-sanitize');
+
+
 
 const app=express();
 
 app.set('query parser',(str)=>qs.parse(str))
 
 //1)Global Middleware 
+
+//set Security HTTP headers
+ app.use(helmet())
+
+ 
+ //Development Logging
 if(process.env.NODE_ENV==='development'){
   app.use(morgan('dev'))
 }
 
+//Limit requests from same API
 //we basically set that 100 times only a user can access in per hour 
 const limiter=rateLimit({
   max:100,
@@ -28,17 +44,35 @@ const limiter=rateLimit({
  app.use('/api',limiter)
 
 
-app.use(express.json())
+//Body Parser, reading data from body into req.body
+app.use(express.json({limit:'10kb'}))//only 10 kilo byte we will allow data to be in req.body
 
+
+//Data Sanitization aganist NoSQL query injection  ("email":{"$gt":""})
+//app.use(mongoSanitize())
+
+//Data Sanitization against XSS
+//this middleware will clean anu user input from the malicious HTML
+//app.use(xss())
+
+//prevent parameter pollution
+app.use(hpp({
+  whitelist:['duration','ratingsQuantity','ratingsAverage','difficulty','maxGroupSize','price']
+}))
+
+//Serving static File
 app.use(express.static(`${__dirname}/public`))
 
+
+//Test Middleware
 app.use((req, res, next) => {
   req.requestTime=new Date().toISOString();
   next(); // Continue
 });
 
- app.use('/api/v1/tours',tourRouter)
 
+//3)Routes
+ app.use('/api/v1/tours',tourRouter)
  app.use('/api/v1/users',userRouter)
 
 
